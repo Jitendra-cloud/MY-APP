ee43c213646490d4b146dda510ce129fcd2884b1
Jitendra Singh <jhonygautam@gmail.com>
2024-10-26 11:35:53 +0530

    Updated 26/10/24=>11.35AM

--- src/MyApp.tsx
+++ src/MyApp.tsx
@@ -1,3 +1,11 @@
+import { Card, CardHeader, Text } from "@ui5/webcomponents-react";
+
 export function MyApp() {
-    return <div>My root component</div>;
-  }  
+  return (
+    <div>
+      <Card header={<CardHeader titleText="Card" />} style={{ width: "300px" }}>
+        <text>This is the content area of the Card</text>
+      </Card>
+    </div>
+  );
+}
